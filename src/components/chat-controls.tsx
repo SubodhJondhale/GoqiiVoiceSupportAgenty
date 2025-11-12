c63b76c90a875d0111e954f882e5ebb7f5f45d39
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2025-11-12 20:32:42 +0530

    Update chat-controls.tsx

--- src/components/chat-controls.tsx
+++ src/components/chat-controls.tsx
@@ -17,12 +17,13 @@ export function ChatControls({
     <div className="absolute top-2 left-2 right-2 flex justify-between">
       <div className="flex gap-2 pl-2">
         <ConfigurationFormDrawer>
-         <Button
+          <Button
             variant="outline"
             size="newSize"
             className="lg:hidden bg-white bg-opacity-70"
           >
             <Settings className="h-10 w-10 text-black opacity-60" />
+          </Button>
         </ConfigurationFormDrawer>
       </div>
       {/* <div className="flex gap-2">
